a9cc15af9170cef32b473a29d932e076be2b85a5
Mrs Sharmin Akter <zamalzamalhossain@gmail.com>
2021-03-25 12:06:00 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const MongoClient = require('mongodb').MongoClient;
+const objectId = require('mongodb').objectId;
+
+const password = 'test123';
+
+const uri = "mongodb+srv://organicUser:test123@cluster0.9e8bh.mongodb.net/organicdb?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/index.html')
+})
+
+
+client.connect(err => {
+    // console.log(err)
+  const productCollection = client.db("organicdb").collection("products");
+
+  app.get('/products', (req, res) => {
+    productCollection.find({})
+    .toArray(  (err, documents)=>{
+        console.log(err, documents)
+        res.send(documents);
+    })
+  });
+
+app.get('/product/:id', (req, res)=>{
+    productCollection.find({_id: objectId(req.params.id)})
+    .toArray( (err, documents)=> {
+        res.send(documents[0]);
+    })
+})
+
+
+ app.post("/addProduct", (req, res) => {
+     const product = req.body;
+     productCollection.insertOne(product)
+     .then(result =>{
+         console.log('data added success');
+        res.redirect('/');
+     })
+ 
+ });
+
+
+ app.patch('/update/:id', (req, res) =>{
+     productCollection.updateOne({_id:objectId(req.params.id)},
+     {
+         $set:{price:req.body.price, quantity: req.body.quantity}
+     })
+     .then(result =>{
+       res.send(result.modifiedCount > 0)
+     })
+ })
+
+
+ app.delete('/delete/:id', (req, res)=>{
+    //  console.log(req.params.id);
+    productCollection.deleteOne({_id:ojectId(req.params.id)})
+    .then(result =>{
+       res.send(result.deletedCount > 0);
+    })
+ })
+
+});
+app.listen(3000,()=>console.log('hello'));
